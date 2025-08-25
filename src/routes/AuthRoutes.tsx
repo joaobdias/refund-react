28e473d09f87aa1b4327e6bb52fe7e158d8cf993
joaobdias <joaobdiasbr@gmail.com>
2025-08-25 12:56:52 -0300

    feat: define authorization endpoints

---
+++ src/routes/AuthRoutes.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router";
+import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
+import { NotFound } from "../pages/NotFound";
+import { AuthLayout } from "../components/AuthLayout";
+
+export function AuthRoutes(){
+    return (
+        <Routes>
+            <Route path="/" element={<AuthLayout/>}>
+                <Route path="/" element={<SignIn/>}/>
+                <Route path="/signup" element={<SignUp/>}/>
+            </Route>
+
+            <Route path="/*" element={<NotFound/>}/>
+        </Routes>
+    )
+}
