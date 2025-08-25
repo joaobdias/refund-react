ded52e8367908c28c764fce5d5f87a4811f036d6
joaobdias <joaobdiasbr@gmail.com>
2025-08-25 20:43:55 -0300

    feat: create employee endpoints

---
+++ src/components/Select.tsx
@@ -0,0 +1,17 @@
+type Props = React.ComponentProps<"select"> & {
+    legend?: string
+}
+
+export function Select({legend, children, ...rest}: Props){
+    return (
+        <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
+            {legend && (<legend className="uppercase text-xxs mb-2 text-inherit">{legend}</legend>)}
+            <select value="" className="w-full h-12 rounded-lg border-gray-300 px-4 text-sm text-gray-100 bg-transparent outline-1 outline-gray-300 focus:border-2 focus:border-green-100 placeholder-gray-300" {...rest}>
+                <option value="" disabled hidden>
+                    Selecione
+                </option>
+                {children}
+            </select>
+        </fieldset>
+    )
+}
