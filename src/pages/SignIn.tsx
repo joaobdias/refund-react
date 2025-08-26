788f855403b1d3a84d803c4d1eb903c61e61c5f2
joaobdias <joaobdiasbr@gmail.com>
2025-08-26 16:25:10 -0300

    feat: connect login endpoint with fontend

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,21 +1,44 @@
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
-import { useState } from "react"
+import { useActionState } from "react"
+import { z, ZodError} from "zod"
+import { AxiosError, Axios } from "axios"
+import { api } from "../services/api"
+
+const signInScheme = z.object ({
+    email: z.string().email({message: "Digite um email válido"}).trim(),
+    password: z.string().trim().min(1, {message: "Informe uma senha"})
+})  
 
 export function SignIn(){
+    
+    const [state, formAction, isLoading] = useActionState(onAction, null)
 
-    const [email, setEmail] = useState("")
-    const [pass, setPass] = useState("")
-    const [load, setIsLoad] = useState(false)
+    async function onAction(_: any, formData: FormData){
+        try {
 
-    function onSubmit(e: React.FormEvent){
-        e.preventDefault()
+            const data = signInScheme.parse({
+            email: formData.get("email"),
+            password: formData.get("password")
+            
+        }) 
+
+        const response = await api.post("/sessions", data)
+        
+        } catch (error) {
+            if(error instanceof ZodError) return {message: error.issues[0].message}
+            if(error instanceof AxiosError) return {message: error.response?.data.message}
+            return {message: "Não foi possível fazer o login"} //state from useACtionState always catch the return
+        }
+
+
     }
 
-    return <form onSubmit={onSubmit} className="w-full flex flex-col gap-4"> {/*onSubmit in form just to trigger with Enter too*/}
-        <Input required legend="E-mail" type="email" placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)}/>
-        <Input required legend="Senha" type="password" placeholder="senha" onChange={(e) => setPass(e.target.value)}/>
-        <Button type="submit" isLoading={load}>Entrar</Button>
+    return <form action={formAction} className="w-full flex flex-col gap-4"> {/*onSubmit in form just to trigger with Enter too*/}
+        <Input name="email" required legend="E-mail" type="email" placeholder="seu@email.com"/>
+        <Input name="password" required legend="Senha" type="password" placeholder="senha"/>
+        <p className="text-sm text-red-600 text-center my-4 font-medium">{state?.message}</p>
+        <Button type="submit" isLoading={isLoading}>Entrar</Button>
         <a href="/signup" className="text-sm font-semibold text-gray-100 mt-5 mb-4 text-center hover:text-green-800 transition ease-linear">Criar conta</a>
     </form>
 }
