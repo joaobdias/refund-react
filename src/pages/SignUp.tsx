0c4741da097d39b23118512b8ef13175bfc28ca8
joaobdias <joaobdiasbr@gmail.com>
2025-08-26 14:57:10 -0300

    fix: change setState of setPassConfirm to the correct one

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,14 @@
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 import { useState } from "react"
+import {z, ZodError} from "zod"
+
+const signUpSchema = z.object ({
+    email: z.email({message: "E-mail inválido"}).trim(),
+    name: z.string().trim().min(1, {message: "Informe um nome"}),
+    pass: z.string().min(6, {message: "Informe uma senha de mínimo de 6 caracteres"}),
+    passConfirm: z.string({message: "Confirme a senha"}),
+}).refine((data) => data.pass === data.passConfirm, {message: "Senhas não concidem", path: ["passConfirm"]})
 
 export function SignUp(){
 
@@ -12,13 +20,30 @@ export function SignUp(){
 
     function onSubmit(e: React.FormEvent){
         e.preventDefault()
+
+        try {
+            setIsLoad(true)
+            const data = signUpSchema.parse({
+                email,
+                name,
+                pass,
+                passConfirm
+            })
+
+        } catch (error) {
+            if(error instanceof ZodError) return alert(error.issues[0].message)
+            
+            alert("Não foi possível cadastrar")
+        } finally {
+            setIsLoad(false)
+        }
     }
 
     return <form onSubmit={onSubmit} className="w-full flex flex-col gap-4"> {/*onSubmit in form just to trigger with Enter too*/}
         <Input required legend="E-mail" type="email" placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)}/>
         <Input required legend="Name" placeholder="nome" onChange={(e) => setName(e.target.value)}/>
         <Input required legend="Senha" type="password" placeholder="senha" onChange={(e) => setPass(e.target.value)}/>
-        <Input required legend="Confirmação da senha" type="password" placeholder="senha" onChange={(e) => setPass(e.target.value)}/>
+        <Input required legend="Confirmação da senha" type="password" placeholder="senha" onChange={(e) => setPassConfirm(e.target.value)}/>
         <Button type="submit" isLoading={load}>Criar conta</Button>
         <a href="/" className="text-sm font-semibold text-gray-100 mt-5 mb-4 text-center hover:text-green-800 transition ease-linear">Já tenho uma conta</a>
     </form>
